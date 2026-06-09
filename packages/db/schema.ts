7deeddf655f9c3df0bb1ae55aa6c11603d54c566
thegladiator16 <161946724+thegladiator16@users.noreply.github.com>
2026-06-09 23:06:26 +0530

    fix: proper db package

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,1 +1,8 @@
-export const schema = {}; export default schema; 
+export const schema = {}
+export default schema
+export const users = {}
+export const campaigns = {}
+export const leads = {}
+export const messages = {}
+export const sequences = {}
+export const analytics = {}
